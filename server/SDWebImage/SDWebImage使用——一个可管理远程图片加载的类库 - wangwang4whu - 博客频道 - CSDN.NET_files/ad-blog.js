d22e16c830c1a56c0d1f09f8cc70f90a275d8eea
rookieZhou <fatjay2046@163.com>
2014-06-18 17:23:06 +0800

    添加sdwebImage
    sdwebImage

---
+++ server/SDWebImage/SDWebImage使用——一个可管理远程图片加载的类库 - wangwang4whu - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,34 @@
+﻿var ad_type = 'ms836';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/**/
+ ad_type = 'ms845_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms815_3';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms819_4';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/**/
+ ad_type = 'ms846_5';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
