4dbb6659fe2ada5fea83b3c0ed357b238ccb0624
rookieZhou <fatjay2046@163.com>
2014-06-25 15:01:25 +0800

    notification object方法
    [notification object]

---
+++ view/UITableViewCell的separatorInset属性 - 标哥--iOS攻城狮 - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,35 @@
+﻿var ad_type = 'ms854';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms857_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms863_3';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms864_4';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/**//*
+ ad_type = 'ms846_5';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+*/
